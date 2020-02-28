f2742e29d410ecc2feb0fd29056c9a7623acd3b7
Fatma <fceyda@gmail.com>
2020-02-28 10:36:02 -0800

    create Pastry.cs logic to update pastry amount and calculate total price of pastries

--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -2,11 +2,36 @@ namespace Models
 {
   public class Pastry
   {
+    public int PastryAmount {get; set;}
+    public int PastryPrice {get; set;}
+    public int PastryTotalPrice {get; set;}
+    public string PastryUpdate {get; set;}
+    
     public Pastry()
     {
-
+      
+      PastryAmount = 0;
+      PastryPrice = 5;
+      PastryTotalPrice = 0;
+      PastryUpdate = "";
     }
 
-    
+      public void PastryOrder(int inputtedAmount)
+      {
+        AddPastry(inputtedAmount);
+        CalculateTotal();
+      }
+      
+      private void AddPastry(int inputtedAmount)
+      {
+        PastryAmount += inputtedAmount;
+      }
+
+      private void CalculateTotal()
+      {
+      PastryTotalPrice = PastryAmount * PastryPrice;
+      PastryUpdate =$"{PastryAmount} pastry = ${PastryTotalPrice}";
+      }
+
   }
 }
