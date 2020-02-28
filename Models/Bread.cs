28a1f118d5a799a396a14c1763b65b7b3f938155
Fatma <fceyda@gmail.com>
2020-02-28 10:02:57 -0800

    create Bread methods to calculate bread's total price

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -2,11 +2,36 @@ namespace Models
 {
   public class Bread
   {
+    public int BreadAmount {get; set;}
+    public int BreadPrice {get; set;}
+    public int BreadTotalPrice {get; set;}
+    public string BreadUpdate {get; set;}
+    
     public Bread()
     {
-
+      BreadAmount = 0;
+      BreadPrice = 5;
+      BreadTotalPrice = 0;
+      BreadUpdate = "";
     }
 
-    
+    public void BreadOrder()
+    {
+      AddBread();
+      CalculateTotal();
+    }
+
+    private void AddBread()
+    {
+      BreadAmount ++;
+    }
+
+    private void CalculateTotal()
+    {
+     BreadTotalPrice = BreadAmount * BreadPrice;
+     BreadUpdate =$"{BreadAmount} loaves of Bread = ${BreadTotalPrice}";
+    }
+
+
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 using System;
+using Models;
 
 namespace Bakery
 {
   public class Program
   {
 
-    public static Main()
+    public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery");
-      Console.WriteLine("Bread: " + + "Pastry: "   );
+      Console.WriteLine("Bread: Pastry: "   );
       // add Price for both
+      Console.WriteLine("Start your order: (Press 'Enter')");
+      Console.ReadLine();
+      Console.WriteLine("Would you like to order Bread or Pastry? (to order bread, type:  'bread'-- to order pastry, type: 'pastry') ");
+      Console.ReadLine();
     }
 
 
